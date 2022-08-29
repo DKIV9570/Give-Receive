a1a40c0f6e4c1f232d27fa9938c55aac45879504
Jin <dkiv9570@gmail.com>
2022-08-29 16:44:28 -0700

    Intro page

--- app/src/main/java/com/example/givereceive/SplashActivity.kt
+++ app/src/main/java/com/example/givereceive/SplashActivity.kt
@@ -1,11 +1,36 @@
 package com.example.givereceive
 
+import android.content.Intent
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import com.example.givereceive.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private var binding: ActivitySplashBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        val typeFace: Typeface = Typeface.createFromAsset(assets,"carbon bl.ttf")
+        binding?.tvAppName?.typeface = typeFace
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,IntroActivity::class.java))
+            finish()
+        }, 2500)
+
+
     }
+
 }
