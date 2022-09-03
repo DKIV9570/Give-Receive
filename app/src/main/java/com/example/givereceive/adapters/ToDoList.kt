7544083c1e7322bca69c67c5b37473e9bbf131fc
Jin <dkiv9570@gmail.com>
2022-09-02 21:29:25 -0700

    some change on ui

--- app/src/main/java/com/example/givereceive/adapters/ToDoList.kt
+++ app/src/main/java/com/example/givereceive/adapters/ToDoList.kt
@@ -4,16 +4,15 @@ class ToDoList {
     // TODO: Bugs:
     //Todo 1. When editing the user info, it only can be done when all the fields are filled in
     //Todo 2. The giving and receiving list, if it is empty or only have 1 item, will not run properly
-    //Todo 3.
+    //Todo 3. The order of posts seems to be random
 
 
     // TODO: features:
-    // todo 1. use the image as background for the post card view
-    // todo 2. Search by give list, Search by receive list
-    // todo 3. Post detail page
+    // todo. add date/time for a post
     // todo 4. Post location
     // todo 5. share to social media
     // todo 6. Google map view for nearby posts
     // todo 7. chatting system
     // todo. My posts with delete button
+    // todo. Multiple language support. Chinese. for exp
 }
